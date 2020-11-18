b30f11a89b879eb9ea992f4b40927a373f0b54bd
pkubickii <kubicki.przemyslaw@gmail.com>
2020-11-18 13:09:52 +0100

    Initial project with graphic ui and integration with maven

---
+++ src/main/java/pl/pkubicki/PrimaryController.java
@@ -0,0 +1,67 @@
+package pl.pkubicki;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.formats.FunctionalSyntaxDocumentFormat;
+import org.semanticweb.owlapi.model.*;
+
+public class PrimaryController {
+    private OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+
+    @FXML
+    private void switchToSecondary() throws IOException {
+        App.setRoot("secondary");
+    }
+    @FXML
+    private void showOwl(ActionEvent actionEvent) {
+        File file = new File("C:\\Users\\pkubicki\\IntelliJIDEAProjects\\PathPlayer\\src\\main\\java\\pl\\pkubicki\\CityOnto.owl");
+        try {
+            OWLOntology ontology = manager.loadOntologyFromOntologyDocument(file);
+            System.out.println("Test buttona." + manager.getOntologies().size());
+            System.out.println(ontology);
+        } catch (OWLOntologyCreationException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void saveOwl(ActionEvent actionEvent) {
+        File fileOut = new File("C:\\Users\\pkubicki\\IntelliJIDEAProjects\\PathPlayer\\src\\main\\java\\pl\\pkubicki\\CityOntoSave.owl");
+        File file = new File("C:\\Users\\pkubicki\\IntelliJIDEAProjects\\PathPlayer\\src\\main\\java\\pl\\pkubicki\\CityOnto.owl");
+        try {
+            OWLOntology ontology = manager.loadOntologyFromOntologyDocument(file);
+            manager.saveOntology(ontology, new FunctionalSyntaxDocumentFormat(), new FileOutputStream(fileOut));
+        } catch (OWLOntologyCreationException | FileNotFoundException | OWLOntologyStorageException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    private void loadTestOWL(ActionEvent actionEvent) {
+        File fileOut = new File("C:\\Users\\pkubicki\\IntelliJIDEAProjects\\PathPlayer\\src\\main\\java\\pl\\pkubicki\\TestOnto.owl");
+        IRI testOnto = IRI.create("http://www.cs.man.ac.uk/~stevensr/ontology/family.rdf.owl");
+        OWLOntology familyOnto = null;
+        try {
+            familyOnto = manager.loadOntology(testOnto);
+            manager.saveOntology(familyOnto, new FunctionalSyntaxDocumentFormat(), new FileOutputStream(fileOut));
+        } catch (OWLOntologyCreationException | FileNotFoundException | OWLOntologyStorageException e) {
+            e.printStackTrace();
+        }
+        System.out.println(familyOnto);
+    }
+    @FXML
+    private void createEmptyOWL(ActionEvent actionEvent) {
+        File fileOut = new File ("C:\\Users\\pkubicki\\IntelliJIDEAProjects\\PathPlayer\\src\\main\\java\\pl\\pkubicki\\EmptyOnto.owl");
+        OWLOntology o;
+        try {
+            o = manager.createOntology();
+            System.out.println(o);
+        } catch (OWLOntologyCreationException e) {
+            e.printStackTrace();
+        }
+    }
+}
